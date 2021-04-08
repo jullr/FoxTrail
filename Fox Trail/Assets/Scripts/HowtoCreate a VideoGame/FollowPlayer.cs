e618c58423e2c4663a78ff72b80da8b26a108ea8
JuliaLaari <julia.laari@hotmail.com>
2021-04-08 20:07:53 +0300

    sideway movement, colliders, forward movement working
    also scene change, end scene

--- Fox Trail/Assets/Scripts/HowtoCreate a VideoGame/FollowPlayer.cs
+++ Fox Trail/Assets/Scripts/HowtoCreate a VideoGame/FollowPlayer.cs
@@ -7,7 +7,7 @@ public class FollowPlayer : MonoBehaviour
 
     public Transform player;
     public Vector3 offset;
-
+    public Vector3 offsetRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,12 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.rotation = player.rotation;
         transform.position = player.position + offset;
+
+        // if(player.rotation < 0f){
+
+        //}
+        
     }
 }
--- Fox Trail/Assets/Scripts/HowtoCreate a VideoGame/PlayerMovement.cs
+++ Fox Trail/Assets/Scripts/HowtoCreate a VideoGame/PlayerMovement.cs
@@ -4,34 +4,78 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    // public Rigidbody rb;
+    // public float forwardForce = 2000f;
+    // public float sidewaysForce = 500f;
 
-    public float jumpSpeed = 8.0f;
+    // public float jumpSpeed = 8.0f;
 
     private Vector3 moveDirection = Vector3.zero;
+
+    private CharacterController controller;
+    // private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float forwardSpeed = 10.0f;
+    // public float jumpHeight = 1.0f;
+    // public float gravityValue = -9.81f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0,0,forwardForce * Time.deltaTime);
-
-        if (Input.GetKey("d")){
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if (Input.GetKey("a")){
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if (rb.position.y < -1f){
-            FindObjectOfType<GameManager>().EndGame();
-        }
-
-        if (Input.GetKey("w")){
-            rb.AddForce(0, jumpSpeed * Time.deltaTime, 0, ForceMode.VelocityChange);
+    
+    //eulerit liikuttaa asteittain, esim 0, 45, 90...
+        //    if(Input.GetAxis("Horizontal") > 0){
+        //     transform.localRotation =  Quaternion.Euler(new Vector3(0, 0, Input.GetAxis("Horizontal")));
             
-        }
+        //    }else if(Input.GetAxis("Horizontal") < 0){
+        //     transform.localRotation = Quaternion.Euler(new Vector3(0, 0, Input.GetAxis("Horizontal")));
+        //    }
+
+        //movement and jump
+        // moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // moveDirection = transform.TransformDirection(moveDirection);
+            if (Input.GetAxis("Horizontal") > 0) {
+                transform.Rotate(0, 0, -playerSpeed);
+            }else if (Input.GetAxis("Horizontal") < 0) {
+                transform.Rotate(0, 0, playerSpeed);
+            }
+
+        // moveDirection = new Vector3(0,0, forwardSpeed);
+        // controller.Move(moveDirection * Time.deltaTime);
+        transform.position += (transform.forward * forwardSpeed ) * Time.deltaTime;
+
+        // moveDirection *= playerSpeed; 
+        // if(Input.GetButton("Jump")){
+        //     moveDirection.y= jumpHeight;
+        // }
+        
+        // // moveDirection.y -= gravityValue * Time.deltaTime;
+        // controller.Move(moveDirection * Time.deltaTime);
+
+
+        // //key presses d and a yms. 
+        // rb.AddForce(0,0,forwardForce * Time.deltaTime);
+
+        // if (Input.GetKey("d")){
+        //     rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        // }
+
+        // if (Input.GetKey("a")){
+        //     rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        // }
+
+        // if (rb.position.y < -1f){
+        //     FindObjectOfType<GameManager>().EndGame();
+        // }
+
+        // if (Input.GetKey("w")){
+        //     rb.AddForce(0, jumpSpeed * Time.deltaTime, 0, ForceMode.VelocityChange);
+            
+        // }
     }
 }
