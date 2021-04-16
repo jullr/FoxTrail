35f963205916d1cc7963833a1d3ab44553056048
JuliaLaari <julia.laari@hotmail.com>
2021-04-16 11:50:15 +0300

    better jump added to script

--- Fox Trail/Assets/Scripts/EndlessTemple/[Unity Tutorial] First Person Controller Spherical Worlds/FirstPersonController.cs
+++ Fox Trail/Assets/Scripts/EndlessTemple/[Unity Tutorial] First Person Controller Spherical Worlds/FirstPersonController.cs
@@ -5,35 +5,35 @@ using System.Collections;
 public class FirstPersonController : MonoBehaviour {
 	
 	// public vars
-	public float mouseSensitivityX = 1;
-	public float mouseSensitivityY = 1;
+	// public float mouseSensitivityX = 1;
+	// public float mouseSensitivityY = 1;
 	public float walkSpeed = 6;
 	public float jumpForce = 220;
 	public LayerMask groundedMask;
-	
+
 	// System vars
 	bool grounded;
 	Vector3 moveAmount;
 	Vector3 smoothMoveVelocity;
 	float verticalLookRotation;
 	Transform cameraTransform;
-	Rigidbody rigidbody;
+	Rigidbody rb;
 	
 	
 	void Awake() {
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 		cameraTransform = Camera.main.transform;
-		rigidbody = GetComponent<Rigidbody> ();
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	void Update() {
 		
-		// Look rotation:
-		transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
-		verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
-		verticalLookRotation = Mathf.Clamp(verticalLookRotation,-60,60);
-		cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+		// // Look rotation:
+		// transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+		// verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+		// verticalLookRotation = Mathf.Clamp(verticalLookRotation,-60,60);
+		// cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
 		
 		// Calculate movement:
 		float inputX = Input.GetAxisRaw("Horizontal");
@@ -43,13 +43,14 @@ public class FirstPersonController : MonoBehaviour {
 		Vector3 targetMoveAmount = moveDir * walkSpeed;
 		moveAmount = Vector3.SmoothDamp(moveAmount,targetMoveAmount,ref smoothMoveVelocity,.15f);
 		
-		// Jump
+		//...............................................................................
+		// Basic Jump
 		if (Input.GetButtonDown("Jump")) {
 			if (grounded) {
-				rigidbody.AddForce(transform.up * jumpForce);
+				rb.AddForce(transform.up * jumpForce);
 			}
 		}
-		
+		//.......................................................................................
 		// Grounded check
 		Ray ray = new Ray(transform.position, -transform.up);
 		RaycastHit hit;
@@ -66,6 +67,6 @@ public class FirstPersonController : MonoBehaviour {
 	void FixedUpdate() {
 		// Apply movement to rigidbody
 		Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
-		rigidbody.MovePosition(rigidbody.position + localMove);
+		rb.MovePosition(rb.position + localMove);
 	}
 }
